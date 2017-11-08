90fa5035930f9b1e6de2a3be5cf3baf47a4aff0f
Steps.Ding <372258@qq.com>
2017-11-08 16:49:21 +0800

    pm2 deploy config

---
+++ ecosystem.config.js
@@ -0,0 +1,52 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+
+    // First application
+    {
+      name      : 'API',
+      script    : './dist/index.js',
+      watch     : true,
+      env: {
+        NODE_ENV: 'development',
+        DEBUG: '*'
+      },
+      env_production : {
+        NODE_ENV: 'production'
+      }
+    }
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy : {
+    production : {
+      user : 'root',
+      host : 'www.aipatn.com',
+      ref  : 'origin/master',
+      repo : 'https://github.com/gogokoala/aipatn-api.git',
+      path : '/mnt/disk1/www/aipatn/api/prod',
+      'post-deploy' : 'npm install && pm2 startOrRestart ecosystem.config.js --env production',
+      env  : {
+        NODE_ENV: 'production'
+      }
+    },
+    dev : {
+      user : 'root',
+      host : 'www.aipatn.com',
+      ref  : 'origin/master',
+      repo : 'https://github.com/gogokoala/aipatn-api.git',
+      path : '/mnt/disk1/www/aipatn/api/dev',
+      'post-deploy' : 'npm install && pm2 startOrRestart ecosystem.config.js --env dev',
+      env  : {
+        NODE_ENV: 'development',
+        DEBUG: '*'
+      }
+    }
+  }
+};
